64cc823690fe5da11fb9c68d4b58300d7a56ad50
Ranagol <andorhorvat@gmail.com>
2023-10-21 12:44:22 +0200

    So far all good.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,23 +8,93 @@
         <title>Home tab title</title>
     </Head>
 
-    <!-- We are sending this h1 to a slot in Layout -->
-    <h1 class="text-4xl font-bold">Page: HOME</h1>
+    <v-card
+        title="Page: HOME"
+        text="This is content"
+    ></v-card>
 
-    
+        <VForm
+    ref="refForm"
+    @submit.prevent="() => {}"
+  >
+    <VRow>
+      <VCol
+        cols="12"
+        md="6"
+      >
+        <AppTextField
+          v-model="name"
+          placeholder="Your Name"
+          persistent-placeholder
+          :rules="[requiredValidator]"
+        />
+      </VCol>
 
+      <VCol
+        cols="12"
+        md="6"
+      >
+        <AppTextField
+          v-model="email"
+          placeholder="Your Email"
+          persistent-placeholder
+          :rules="[requiredValidator, emailValidator]"
+        />
+      </VCol>
+
+      <VCol
+        cols="12"
+        md="6"
+      >
+        <AppTextField
+          v-model="password"
+          type="password"
+          placeholder="Your Password"
+          persistent-placeholder
+          :rules="[requiredValidator, passwordValidator]"
+          autocomplete="on"
+        />
+      </VCol>
+
+      <VCol
+        cols="12"
+        md="6"
+      >
+        <AppTextField
+          v-model="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          persistent-placeholder
+          :rules="[requiredValidator, confirmedValidator(confirmPassword, password)]"
+          autocomplete="on"
+        />
+      </VCol>
+
+      <VCol cols="12">
+
+        <VBtn
+          type="submit"
+          @click="refForm?.validate()"
+        >
+          Submit
+        </VBtn>
+
+      </VCol>
+    </VRow>
+  </VForm>
 
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import type { VForm } from 'vuetify/components/VForm'
+import { confirmedValidator, emailValidator, passwordValidator, requiredValidator } from '@validators';
+import { ref } from 'vue';
 
-export default {
-	name: 'Home',
-	components: {
-	},
-	data() {
-		return {
-		}
-	}
-}
+const name = ref('')
+const email = ref('')
+const refForm = ref<VForm>()
+const password = ref('')
+const confirmPassword = ref('')
+
 </script>
